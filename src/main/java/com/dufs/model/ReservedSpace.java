294cbefaa3a7ffb6ec70e7e4069b2989bc52a8ce
David Utyuganov <singerous@gmail.com>
2022-12-13 00:31:30 +0300

    add ReservedSpace class
    * storage class that is stored in the beginning of the volume and keeps service information of volume/filesystem

--- src/main/java/com/dufs/model/ReservedSpace.java
+++ src/main/java/com/dufs/model/ReservedSpace.java
@@ -1,8 +1,16 @@
 package com.dufs.model;
 
+import com.dufs.utility.DateUtility;
+import com.dufs.utility.VolumeUtility;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class ReservedSpace {
-    private int DUFSNoseSignature = 0x44554653;
-    private long volumeName;
+    private final static int DUFS_NOSE_SIGNATURE = 0x44554653; // "DUFS"
+    private char[] volumeName = new char[8];
+    private int clusterSize;
+    private long volumeSize;
     private int reservedClusters;
     private short createDate;
     private short createTime;
@@ -11,5 +19,19 @@ public class ReservedSpace {
     private int nextClusterIndex;
     private int freeClustersCount;
     private int nextRecordIndex;
-    private int DUFSTailSignature = 0x4A455442;
+    private final static int DUFS_TAIL_SIGNATURE = 0x4A455442; // "JETB"
+
+    public ReservedSpace(char[] volumeName, int clusterSize, long volumeSize) {
+        this.volumeName = volumeName;
+        this.clusterSize = clusterSize;
+        this.volumeSize = volumeSize;
+        this.reservedClusters = VolumeUtility.clustersAmount(clusterSize, volumeSize);
+        this.createDate = com.dufs.utility.DateUtility.dateToShort(LocalDate.now());
+        this.createTime = DateUtility.timeToShort(LocalDateTime.now());
+        this.lastDefragmentationDate = this.createDate;
+        this.lastDefragmentationTime = this.createTime;
+        this.nextClusterIndex = 0;
+        this.freeClustersCount = reservedClusters;
+        this.nextRecordIndex = 0;
+    }
 }
