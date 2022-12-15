c7607d28dbeea4e87bbbdfed7b273122e0da89ce
David Utyuganov <singerous@gmail.com>
2022-12-16 01:06:20 +0300

    remove static word

--- src/main/java/com/dufs/filesystem/SharedData.java
+++ src/main/java/com/dufs/filesystem/SharedData.java
@@ -1,19 +1,18 @@
 package com.dufs.filesystem;
 
 import com.dufs.model.ReservedSpace;
-import com.dufs.offsets.ClusterIndexListOffsets;
 import com.dufs.utility.VolumeUtility;
 
 public class SharedData {
-    private static ReservedSpace reservedSpace;
-    private static long recordListOffset;
+    private ReservedSpace reservedSpace;
+    private long recordListOffset;
 
     public ReservedSpace getReservedSpace() {
         return reservedSpace;
     }
 
     public void setReservedSpace(ReservedSpace reservedSpace) {
-        SharedData.reservedSpace = reservedSpace;
+        this.reservedSpace = reservedSpace;
     }
 
     public long getRecordListOffset() {
@@ -21,15 +20,15 @@ public class SharedData {
     }
 
     public void setRecordListOffset(long recordListOffset) {
-        SharedData.recordListOffset = recordListOffset;
+        this.recordListOffset = recordListOffset;
     }
 
     public SharedData(ReservedSpace reservedSpace) {
-        SharedData.reservedSpace = reservedSpace;
+        this.reservedSpace = reservedSpace;
         recordListOffset = VolumeUtility.calculateRecordListOffset(reservedSpace);
     }
 
-    public static void updateNextClusterIndex(int nextClusterIndex) {
+    public void updateNextClusterIndex(int nextClusterIndex) {
         reservedSpace.setNextClusterIndex(nextClusterIndex);
     }
 }
