c61dee127a0ad22d45a417cd6520b4def0685660
David Utyuganov <singerous@gmail.com>
2022-12-16 23:49:43 +0300

    create RecordOffset class

---
+++ src/main/java/com/dufs/offsets/RecordOffsets.java
@@ -0,0 +1,13 @@
+package com.dufs.offsets;
+
+public class RecordOffsets {
+    public static final int NAME_OFFSET = 0;
+    public static final int CREATE_DATA_OFFSET = 64;
+    public static final int CREATE_TIME_OFFSET = 66;
+    public static final int FIRST_CLUSTER_INDEX_OFFSET = 68;
+    public static final int LAST_EDIT_DATE_OFFSET = 72;
+    public static final int LAST_EDIT_TIME_OFFSET = 74;
+    public static final int SIZE_OFFSET = 76;
+    public static final int PARENT_DIRECTORY_INDEX_OFFSET = 84;
+    public static final int IS_FILE_OFFSET = 88;
+}
