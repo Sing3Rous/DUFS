43f5e876df78a5eefeb6d889a9e736ff9e7a6775
David Utyuganov <singerous@gmail.com>
2022-12-18 02:14:19 +0300

    fix typo

--- src/main/java/com/dufs/offsets/RecordOffsets.java
+++ src/main/java/com/dufs/offsets/RecordOffsets.java
@@ -2,7 +2,7 @@ package com.dufs.offsets;
 
 public class RecordOffsets {
     public static final int NAME_OFFSET = 0;
-    public static final int CREATE_DATA_OFFSET = 64;
+    public static final int CREATE_DATE_OFFSET = 64;
     public static final int CREATE_TIME_OFFSET = 66;
     public static final int FIRST_CLUSTER_INDEX_OFFSET = 68;
     public static final int LAST_EDIT_DATE_OFFSET = 72;
