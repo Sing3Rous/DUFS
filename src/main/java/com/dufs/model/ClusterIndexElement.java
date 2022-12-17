0cb9ae948302a9ed85d7422f59ec63697d12a0e2
David Utyuganov <singerous@gmail.com>
2022-12-18 02:13:49 +0300

    add getters/setters

--- src/main/java/com/dufs/model/ClusterIndexElement.java
+++ src/main/java/com/dufs/model/ClusterIndexElement.java
@@ -6,6 +6,22 @@ public class ClusterIndexElement {
     private int nextClusterIndex;
     private int prevClusterIndex;
 
+    public int getNextClusterIndex() {
+        return nextClusterIndex;
+    }
+
+    public void setNextClusterIndex(int nextClusterIndex) {
+        this.nextClusterIndex = nextClusterIndex;
+    }
+
+    public int getPrevClusterIndex() {
+        return prevClusterIndex;
+    }
+
+    public void setPrevClusterIndex(int prevClusterIndex) {
+        this.prevClusterIndex = prevClusterIndex;
+    }
+
     public byte[] serialize() {
         final int bytesCount = 8;
         ByteBuffer buffer = ByteBuffer.allocateDirect(bytesCount);
