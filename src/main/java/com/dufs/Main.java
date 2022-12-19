c536706309111e2b1ab96ae977f1b76b67221387
David Utyuganov <singerous@gmail.com>
2022-12-20 01:36:50 +0300

    prepared main() to ready-to-test condition

--- src/main/java/com/dufs/Main.java
+++ src/main/java/com/dufs/Main.java
@@ -5,15 +5,25 @@ import com.dufs.filesystem.Dufs;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystems;
 
 public class Main {
     public static void main(String[] args) throws IOException, DufsException {
         Dufs dufs = new Dufs();
         dufs.mountVolume("DISK_B", 4096, 4096000);
         //dufs.attachVolume("DISK_B");
+        dufs.printVolumeInfo();
+        dufs.createFile("DISK_B", "LOL");
+        dufs.createFile("DISK_B", "ZOZA");
         dufs.createFile("DISK_B", "qwe");
-        dufs.writeFile("DISK_B\\qwe", new File("abc.txt"));
-        dufs.readFile("DISK_B\\qwe", new File("qwe.txt"));
+        dufs.printDirectoryContent("DISK_B");
+        dufs.writeFile("DISK_B\\LOL", new File("abcde.txt"));
+        dufs.printDirectoryContent("DISK_B");
+        dufs.printVolumeInfo();
+        //dufs.deleteFile("DISK_B\\LOL");
+        dufs.appendFile("DISK_B\\LOL", new File("abc.txt"));
+        dufs.printDirectoryContent("DISK_B");
+        dufs.createFile("DISK_B", "kolpak");
+        dufs.printDirectoryContent("DISK_B");
+        dufs.readFile("DISK_B\\LOL", new File("LOL.txt"));
     }
 }
